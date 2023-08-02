a78c8443ad8dcf40a9530ad5bbe172fbfacd6979
kingroxas2 <59804844+kingroxas2@users.noreply.github.com>
2023-08-02 08:49:44 +0800

    Login And Register Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,309 @@
-import 'dart:io';
-
-import 'package:camera/camera.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'homeScreen.dart';
-
-List<CameraDescription> cameras = [];
-List<Widget> imageWidgets = [];
-
-class Report {
-  final String reportId;
-  final String userId;
-  final String title;
-  final String animalType;
-  final String location;
-  final String description;
-  final File imageFile;
-
-  Report({
-    required this.reportId,
-    required this.userId,
-    required this.title,
-    required this.animalType,
-    required this.location,
-    required this.description,
-    required this.imageFile,
-  });
+void main() {
+  runApp(MyApp());
 }
 
-
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  cameras = await availableCameras();
-  runApp(const MyApp());
-}
-
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: const Color.fromARGB(255, 51, 55, 85),
+      title: 'Login and Register',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => LoginPage(),
+        '/register': (context) => RegisterPage(),
+      },
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Login')),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextFormField(
+                  controller: _emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value?.isEmpty ?? true) {
+                      return 'Please enter your email';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  controller: _passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value?.isEmpty ?? true) {
+                      return 'Please enter your password';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+
+                    if (_formKey.currentState?.validate() ?? false) {
+                      // Perform login action here
+                      String email = _emailController.text;
+                      String password = _passwordController.text;
+                      // Implement your login logic here
+                    }
+                  },
+                  child: Text('Login'),
+                ),
+                SizedBox(height: 10),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/register');
+                  },
+                  child: Text('New user? Register here'),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
-      home: const Student(),
+    );
+  }
+}
+
+class RegisterPage extends StatefulWidget {
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _staffIdController = TextEditingController();
+  final TextEditingController _fullNameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _hasImplants = false; // Default value is false
+  bool _takesHormones = false; // Default value is false
+  String _selectedRole = 'patient'; // Default role is patient
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Register')),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Form(
+            key: _formKey,
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  DropdownButtonFormField<String>(
+                    value: _selectedRole,
+                    items: ['patient', 'admin'].map((role) {
+                      return DropdownMenuItem<String>(
+                        value: role,
+                        child: Text(role.toUpperCase()),
+                      );
+                    }).toList(),
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedRole = value!;
+                      });
+                    },
+                    decoration: InputDecoration(
+                      labelText: 'Role',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  if (_selectedRole == 'patient') ...[
+                    TextFormField(
+                      controller: _fullNameController,
+                      decoration: InputDecoration(
+                        labelText: 'Full Name',
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value?.isEmpty ?? true) {
+                          return 'Please enter your full name';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 20),
+                    TextFormField(
+                      controller: _emailController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value?.isEmpty ?? true) {
+                          return 'Please enter your email';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 20),
+                    TextFormField(
+                      controller: _passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value?.isEmpty ?? true) {
+                          return 'Please enter your password';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 20),
+                    CheckboxListTile(
+                      title: Text('Do you have implants?'),
+                      value: _hasImplants ?? false,
+                      onChanged: (value) {
+                        setState(() {
+                          _hasImplants = value ?? false;
+                        });
+                      },
+                    ),
+                    CheckboxListTile(
+                      title: Text('Do you take hormones?'),
+                      value: _takesHormones ?? false,
+                      onChanged: (value) {
+                        setState(() {
+                          _takesHormones = value ?? false;
+                        });
+                      },
+                    ),
+                  ],
+                  SizedBox(height: 20),
+                  if (_selectedRole == 'admin') ...[
+                    TextFormField(
+                      controller: _staffIdController,
+                      decoration: InputDecoration(
+                        labelText: 'Staff ID',
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value?.isEmpty ?? true) {
+                          return 'Please enter Staff ID';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 20),
+                    TextFormField(
+                      controller: _fullNameController,
+                      decoration: InputDecoration(
+                        labelText: 'Full Name',
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value?.isEmpty ?? true) {
+                          return 'Please enter your full name';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 20),
+                    TextFormField(
+                      controller: _emailController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value?.isEmpty ?? true) {
+                          return 'Please enter your email';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 20),
+                    TextFormField(
+                      controller: _passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value?.isEmpty ?? true) {
+                          return 'Please enter your password';
+                        }
+                        return null;
+                      },
+                    ),
+                  ],
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/login');
+                      if (_formKey.currentState?.validate() ?? false) {
+                        // Perform registration action here
+                        String fullName = _fullNameController.text;
+                        // ... (other common fields)
+                        String email = _emailController.text;
+                        String password = _passwordController.text;
+                        bool hasImplants = _hasImplants ?? false;
+                        bool takesHormones = _takesHormones ?? false;
+                        String role = _selectedRole;
+                        // Implement your registration logic here
+                        // Check the role and perform actions accordingly
+                        if (role == 'admin') {
+                          // Admin registration
+                          String staffId = _staffIdController.text;
+                          // Implement admin registration logic here
+                          // You can use staffId for admin registration
+                        } else {
+                          // Patient registration
+                          // Implement patient registration logic here
+                        }
+                      }
+                    },
+                    child: Text('Register'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
