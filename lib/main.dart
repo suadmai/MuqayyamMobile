b1c05ee206ad3243888553b98b1f6d520e1cac85
kingroxas2 <59804844+kingroxas2@users.noreply.github.com>
2023-08-31 01:18:00 +0800

    Delete Report not needed

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,17 @@
 import 'dart:io';
 
-import 'package:camera/camera.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'homeScreen.dart';
 
-List<CameraDescription> cameras = [];
 List<Widget> imageWidgets = [];
-
-class Report {
-  final String reportId;
-  final String userId;
-  final String title;
-  final String animalType;
-  final String location;
-  final String description;
-  final File imageFile;
-
-  Report({
-    required this.reportId,
-    required this.userId,
-    required this.title,
-    required this.animalType,
-    required this.location,
-    required this.description,
-    required this.imageFile,
-  });
-}
-
 
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  cameras = await availableCameras();
+
   runApp(const MyApp());
 }
 
