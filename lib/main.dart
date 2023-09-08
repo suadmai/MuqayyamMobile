9ceed68e6361751c8e47320784133c069793e81e
kingroxas2 <59804844+kingroxas2@users.noreply.github.com>
2023-09-08 14:55:47 +0800

    Update UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,27 @@
 import 'package:flutter/material.dart';
 
-
-
 void main() {
   runApp(MyApp());
 }
 
-class LevelofSurahWidget extends StatelessWidget {
+class LevelofSurahWidget extends StatefulWidget {
   final String level;
   final String leveldesc;
-  final Color backgroundColor;
   final List<String> surahs;
+  final String backgroundImageAsset;
 
   LevelofSurahWidget({
     required this.level,
     required this.leveldesc,
-    required this.backgroundColor,
     required this.surahs,
+    required this.backgroundImageAsset,
   });
 
+  @override
+  State<LevelofSurahWidget> createState() => _LevelofSurahWidgetState();
+}
+
+class _LevelofSurahWidgetState extends State<LevelofSurahWidget> {
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -28,52 +31,68 @@ class LevelofSurahWidget extends StatelessWidget {
       ),
       child: Column(
         mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,  //items to the right
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
             width: double.infinity,
             decoration: BoxDecoration(
-              color: backgroundColor,
               borderRadius: BorderRadius.only(
                 topLeft: Radius.circular(12.0),
                 topRight: Radius.circular(12.0),
+              ),
+              image: DecorationImage(
+                image: AssetImage(widget.backgroundImageAsset),
+                fit: BoxFit.cover,
               ),
             ),
             padding: EdgeInsets.all(16.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                //For background image specification
+
                 Text(
-                  level,
+                  widget.level,
                   style: TextStyle(
                     fontSize: 18.0,
                     fontWeight: FontWeight.bold,
-                    color: Colors.white, // Text color for class name
+                    color: Colors.white,
                   ),
                 ),
                 SizedBox(height: 8.0),
                 Text(
-                  'Teacher: $leveldesc',
+                  'Teacher: ${widget.leveldesc}',
                   style: TextStyle(
                     fontSize: 16.0,
-                    color: Colors.white, // Text color for teacher name
+                    color: Colors.white,
                   ),
                 ),
               ],
             ),
           ),
           Container(
-            color: Colors.white, // Background color for the list of fruits
+            color: Colors.white,
             padding: EdgeInsets.all(16.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
-              children: surahs
-                  .map((surah) => Text(
-                        surah,
-                        style: TextStyle(
+              children: widget.surahs
+                  .map((surah) => GestureDetector(
+                        onTap: () {
+                          //navigate to surah page
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => Placeholder()),
+                          );
+                        },
+                        
+                        child: Text(
+                          surah,
+                          style: TextStyle(
                           fontSize: 14.0,
-                          color: Colors.black, // Text color for fruits
+                          color: Colors.black,
+                          ),
                         ),
+                        
                       ))
                   .toList(),
             ),
@@ -84,124 +103,104 @@ class LevelofSurahWidget extends StatelessWidget {
   }
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:  Scaffold(
-      backgroundColor: Color(0xFFEBEBEB),
-      appBar: AppBar(
-        backgroundColor: const Color(0xFF82618B),
-        title: const Text("Baca Al-Quran"),
-        actions: [
-          IconButton(
-            onPressed: () {
-              //go to profile page
-            },
-            icon: const Icon(
-              Icons.account_circle,
-              size: 30,
-            ),
-          )
-        ],
-      ),
-
-
-      
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          
-        }, 
-        backgroundColor: Color(0xFF82618B),
-        child: const Icon(Icons.podcasts),
-      ),
-
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-
-      
-      
-      body: Center(
-        child: Column(
-          
-          children: [
-
-            SizedBox(height: 16.0), // Added SizedBox
-
-
-
-             LevelofSurahWidget(
-                  level: 'Level 1',
-                  leveldesc: 'Mr. Smith',
-                  backgroundColor: Colors.blue, // Set your desired background color
-                  surahs: ['Apple', 'Banana', 'Orange'],
-                ),
-      
-                SizedBox(height: 16.0), // Added SizedBox
-      
-                LevelofSurahWidget(
-                  level: 'Mathematics',
-                  leveldesc: 'Mr. Smith',
-                  backgroundColor: Color.fromARGB(255, 210, 21, 21), // Set your desired background color
-                  surahs: ['Apple', 'Banana', 'Orange'],
-                ),
-            
-          
-        ],
+      home: Scaffold(
+        backgroundColor: Color(0xFFEBEBEB),
+        appBar: AppBar(
+          backgroundColor: const Color(0xFF82618B),
+          title: const Text("Baca Al-Quran"),
+          actions: [
+            IconButton(
+              onPressed: () {
+                // Go to profile page
+              },
+              icon: const Icon(
+                Icons.account_circle,
+                size: 30,
+              ),
+            )
+          ],
         ),
-      ),
-
-      bottomNavigationBar: BottomAppBar(
-        color: Color(0xFF82618B),
-        shape: const CircularNotchedRectangle(),
-        notchMargin: 10.0,
-        child: SizedBox(
-          height: 60.0,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround, // Updated alignment
-            children: <Widget>[
-              // Home
-              IconButton(
-                onPressed: () {
-                  
-                },
-                icon: const Icon(Icons.home),
-                color: Colors.white,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // Handle floating action button press
+          },
+          backgroundColor: Color(0xFF82618B),
+          child: const Icon(Icons.podcasts),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        body: Center(
+          child: Column(
+            children: [
+              SizedBox(height: 16.0),
+              LevelofSurahWidget(
+                level: 'Level 1',
+                leveldesc: 'Mr. Smith',
+                backgroundImageAsset: 'images/level1.jpg',
+                surahs: ['Apple', 'Banana', 'Orange'],
               ),
-
-              // Search (You can replace this with your desired search functionality)
-              IconButton(
-                onPressed: () {
-                  // Add your search functionality here
-                },
-                icon: const Icon(Icons.search),
-                color: Colors.white,
-              ),
-
-              // Trophy (You can replace this with your desired trophy functionality)
-              IconButton(
-                onPressed: () {
-                  // Add your trophy functionality here
-                },
-                icon: const Icon(Icons.emoji_events),
-                color: Colors.white,
-              ),
-
-              // Settings (You can replace this with your desired settings functionality)
-              IconButton(
-                onPressed: () {
-                  // Add your settings functionality here
-                },
-                icon: const Icon(Icons.settings),
-                color: Colors.white,
+              SizedBox(height: 16.0),
+              LevelofSurahWidget(
+                level: 'Mathematics',
+                leveldesc: 'Mr. Smith',
+                backgroundImageAsset: 'images/level2.jpg',
+                surahs: ['Apple', 'Banana', 'Orange'],
               ),
             ],
           ),
         ),
+        bottomNavigationBar: BottomAppBar(
+          color: Color(0xFF82618B),
+          shape: const CircularNotchedRectangle(),
+          notchMargin: 10.0,
+          child: SizedBox(
+            height: 60.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                IconButton(
+                  onPressed: () {
+                    // Handle home button press
+                  },
+                  icon: const Icon(Icons.home),
+                  color: Colors.white,
+                ),
+                IconButton(
+                  onPressed: () {
+                    // Handle search button press
+                  },
+                  icon: const Icon(Icons.search),
+                  color: Colors.white,
+                ),
+                IconButton(
+                  onPressed: () {
+                    // Handle trophy button press
+                  },
+                  icon: const Icon(Icons.emoji_events),
+                  color: Colors.white,
+                ),
+                IconButton(
+                  onPressed: () {
+                    // Handle settings button press
+                  },
+                  icon: const Icon(Icons.settings),
+                  color: Colors.white,
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
-
-    ),
     );
   }
 }
