90d040f4160bd15fa92d7137f9d3d6f7d891c876
Firdaus Shahrum <firdausshahrum21@gmail.com>
2023-09-15 01:38:50 +0800

    commit 2

--- lib/pages/chat.dart
+++ lib/pages/chat.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import '../firebase/firebase_config.dart';
-import 'firebase/firebase_config.dart';
 
 class Chat extends StatefulWidget {
   final String userID;
