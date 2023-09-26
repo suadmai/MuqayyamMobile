e7d7eba4aa5a4784345ce8c8dee9cb509bf684d8
kingroxas2 <59804844+kingroxas2@users.noreply.github.com>
2023-09-26 12:03:06 +0800

    Update Logo

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:wildlifego/pages/splash_screen.dart';
 import 'package:wildlifego/services/auth_gate.dart';
 import 'package:wildlifego/services/auth_service.dart';
 import 'package:provider/provider.dart';
@@ -19,9 +20,6 @@ List<Widget> imageWidgets = [];
 
 
 Future<void> main() async {
-
-    await Future.delayed(const Duration(seconds: 30));
-  FlutterNativeSplash.remove;
 
   WidgetsFlutterBinding.ensureInitialized();
 
@@ -46,7 +44,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: AuthGate(),
+      home: SplashScreen(),
     );
   }
 }
---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:wildlifego/pages/homeScreen.dart';
+import 'package:wildlifego/services/auth_gate.dart';
+
+import 'home_page.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (_) => AuthGate()));
+    });
+  }
+
+  @override
+  void dispose() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: SystemUiOverlay.values);
+    super.dispose();
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          width: double.infinity,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color.fromARGB(255, 255, 255, 255), Color.fromARGB(255, 250, 216, 255)],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Image(
+                image: AssetImage('images/muqayyam_icon.png'),
+                height: 300,
+                width: 300,
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Selamat Datang ke Muqayyam!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color:Color(0xFF82618B),
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                  
+                ),
+              )
+            ],
+          )),
+    );
+  }
+}
