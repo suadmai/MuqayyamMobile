705a647012a29424780d1861ed85cbec7ca6706d
kingroxas2 <59804844+kingroxas2@users.noreply.github.com>
2023-09-26 12:31:59 +0800

    Fix Logo Error

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:wildlifego/pages/splash_screen.dart';
 import 'package:wildlifego/services/auth_gate.dart';
 import 'package:wildlifego/services/auth_service.dart';
