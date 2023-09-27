604e2766374b5517f9ea6be396c84d994ef03a7d
kingroxas2 <59804844+kingroxas2@users.noreply.github.com>
2023-09-27 21:39:08 +0800

    Done Login and Logo again

--- lib/services/auth_gate.dart
+++ lib/services/auth_gate.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart'; // Import Firestore
 
 import '../pages/doctor/Admin_HomeScreen.dart';
 import '../pages/homeScreen.dart';
@@ -13,23 +13,43 @@ class AuthGate extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: StreamBuilder(
-          stream: FirebaseAuth.instance.authStateChanges(),
-          builder: (context, snapshot) {
-            //user is logged in
-            if (snapshot.hasData) {
-              return const HomeScreen(); //for now open the admin home screen
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          // Check if the user is logged in
+          if (snapshot.hasData) {
+            // User is logged in, fetch their role from Firestore
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              return FutureBuilder<DocumentSnapshot>(
+                future: FirebaseFirestore.instance.collection('users').doc(user.uid).get(),
+                builder: (context, roleSnapshot) {
+                  if (roleSnapshot.connectionState == ConnectionState.done) {
+                    if (roleSnapshot.hasData) {
+                      final userRole = roleSnapshot.data!['role'];
+
+                      // Check user role and navigate accordingly
+                      if (userRole == 'Doktor') {
+                        return const AdminHomeScreen();
+                      } else {
+                        return const HomeScreen();
+                      }
+                    } else {
+                      // Handle error or loading state
+                      return const CircularProgressIndicator(); // Placeholder for loading
+                    }
+                  } else {
+                    // Handle error or loading state
+                    return const CircularProgressIndicator(); // Placeholder for loading
+                  }
+                },
+              );
             }
+          }
 
-            //user is NOT logged in
-            else {
-              return const LoginOrRegister();
-            }
-
-            //user is logged in
-
-            //user is NOT logged in
-          },
-          ),
+          // User is NOT logged in
+          return const LoginOrRegister();
+        },
+      ),
     );
   }
 }
